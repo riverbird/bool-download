0b611eedb3b7818ef4fcd479ef9b079014df900a
hunter <riverbird@aliyun.com>
2026-08-25 10:31:23 +0800

    Update build_deb_desktop.sh

--- scripts/build_deb_desktop.sh
+++ scripts/build_deb_desktop.sh
@@ -4,8 +4,8 @@ echo "准备文件..."
 rm -rf /home/riverbird/builder/bool_download_build_root/usr/local/bool-download/*
 cp -r /home/riverbird/project/bool-download/BoolDownload.Desktop/bin/Release/net10.0/linux-x64/publish/* /home/riverbird/builder/bool_download_build_root/usr/local/bool-download/
 
-cp /home/riverbird/project/bool-download/BoolDownload/Assets/Icon.png /home/riverbird/builder/bool_download_build_root/usr/local/bool-download/
-cp /home/riverbird/project/bool-download/BoolDownload/Assets/Icon.png /home/riverbird/builder/bool_download_build_root/usr/share/icons/
+cp /home/riverbird/project/bool-download/BoolDownload/Assets/Icon.png /home/riverbird/builder/bool_download_build_root/usr/local/bool-download/bool-download-icon.png
+cp /home/riverbird/project/bool-download/BoolDownload/Assets/Icon.png /home/riverbird/builder/bool_download_build_root/usr/share/icons/bool-download-icon.png
 
 cp /home/riverbird/project/bool-download/scripts/bool-download.desktop /home/riverbird/builder/bool_download_build_root/usr/local/bool-download/
 cp /home/riverbird/project/bool-download/scripts/bool-download.desktop /home/riverbird/builder/bool_download_build_root/usr/share/applications/
@@ -13,7 +13,7 @@ cp /home/riverbird/project/bool-download/scripts/bool-download.desktop /home/riv
 echo "开始打包..."
 fpm -s dir -t deb \
   -n booldownload \
-  -v 1.1.0 \
+  -v 1.2.0 \
   --iteration 1.el7 \
   --prefix / \
   --description "基于迅雷下载引擎开发的跨平台下载工具。" \
